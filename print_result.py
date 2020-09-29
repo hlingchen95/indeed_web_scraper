6f04d8ee535f49fc7c9ebd51914ebf45d8b99243
Huiling Chen <hlingchen95@gmail.com>
2020-09-29 18:17:26 -0400

    date formatted
    date formatted

---
+++ print_result.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import numpy as np
+import pandas as pd
+import math
+from datetime import datetime, timedelta
+
+b = pd.read_pickle("result.pkl")
+print(b)
