405025dfea13e8d8bbfcb6f32d412cbbe72fadb7
Jiang Li <jiang.li.email@gmail.com>
2021-01-11 23:08:45 -0500

    get link

--- get_indeed_data.py
+++ get_indeed_data.py
@@ -23,14 +23,7 @@ driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
 
 driver.get("https://www.indeed.com/advanced_search")
 
-# initial_search_button = driver.find_element_by_xpath(
-#     '//*[@id="whatWhereFormId"]/div[3]/button')
-# initial_search_button.click()
-# driver.implicitly_wait(3)
-
-# advanced_search = driver.find_element_by_xpath(
-#     "//a[contains(text(),'Advanced Job Search')]")
-# advanced_search.click()
+driver.implicitly_wait(3)
 
 # search analyst
 search_job = driver.find_element_by_xpath('//input[@id="as_and"]')
@@ -39,134 +32,74 @@ search_job.send_keys(['Analyst'])
 # search location
 searchLocation = driver.find_element_by_xpath('//input[@id="where"]')
 searchLocation.clear()
-searchLocation.send_keys("Columbus,OH")
+searchLocation.send_keys("united states")
+# searchLocation.send_keys("ohio")
 
 # set display limit of 50 results per page
 display_limit = driver.find_element_by_xpath(
     '//select[@id="limit"]//option[@value="50"]')
 display_limit.click()
+driver.implicitly_wait(3)
+
 # sort display with date
 display_sort = driver.find_element_by_xpath(
     '//select[@id="sort"]//option[@value="date"]')
 display_sort.click()
+driver.implicitly_wait(3)
 
-# limited to 7 days
+# limited to 1 day
 result_age = driver.find_element_by_xpath(
-        '//select[@id="fromage"]//option[@value="7"]')
+    '//select[@id="fromage"]//option[@value="1"]')
+result_age.click()
+
+# location
+result_age = driver.find_element_by_xpath(
+    '//select[@id="radius"]//option[@value="0"]')
 result_age.click()
 
 driver.implicitly_wait(3)
+
 # push search button
 search_button = driver.find_element_by_xpath('//*[@id="fj"]')
 search_button.click()
+driver.implicitly_wait(3)
 
 # Get exact search result amount
 search_count = driver.find_element_by_xpath('//div[@id="searchCount"]').text
-pages = math.ceil(int(search_count[10:13])/50)
 
-print("search count: ", search_count)
-print("pages: ", pages)
+def sub_str(s, start, end):
+    return s[s.find(start)+len(start):s.rfind(end)]
+num = sub_str(search_count, "of ", " jobs")
+pages = math.ceil(int(num.replace(',', ''))/50)
 
-titles = []
-companies = []
+print(datetime.now().strftime("%m_%d_%Y"))
+print("total pages: ", pages)
+
 links = []
-salaries = []
-locations = []
-descriptions = []
 dates = []
 
 for i in range(0, pages):
 
     job_card = driver.find_elements_by_xpath(
         '//div[contains(@class,"clickcard")]')
-
+    
     for job in job_card:
-
-        # Title
-        try:
-            title = job.find_element_by_xpath('.//h2[@class="title"]//a').text
-        except:
-            title = job.find_element_by_xpath(
-                './/h2[@class="title"]//a').get_attribute(name="title")
-        titles.append(title)
-
-        # link
-        links.append(job.find_element_by_xpath(
-            './/h2[@class="title"]//a').get_attribute(name="href"))
-        # company
-        companies.append(job.find_element_by_xpath(
-            './/span[@class="company"]').text)
-
-        # salary
-        try:
-            salary = job.find_element_by_xpath(
-                './/span[@class="salaryText"]').text
-        except:
-            salary = "None"
-
-        salaries.append(salary)
-
-        # location
-        try:
-            location = job.find_element_by_xpath(
-                './/span[contains(@class,"location")]').text
-        except:
-            location = "None"
-
-        locations.append(location)
-
-        # date posted
-        try:
-            date = job.find_element_by_xpath('.//span[@class="date "]').text
-            word_list = date.split()
-
-            if word_list[0].isdigit():
-                num = int(word_list[0])
-                new_date_prior = datetime.now() - timedelta(days=num)
-                date = new_date_prior.strftime("%m/%d/%Y")
-            elif word_list[0] == '30+':
-                new_date_prior = datetime.now()-timedelta(days=30)
-                date = new_date_prior.strftime("%m/%d/%Y")+" prior"
-            else:
-                date = datetime.now().strftime("%m/%d/%Y")
-        except:
-            date = "None"
-
+        
+        date = datetime.now().strftime("%m/%d/%Y")
         dates.append(date)
 
-    # Flip to next page
-    try:
-        next_page = driver.find_element_by_xpath(
-            '//a[@aria-label={}]//span[@class="pn"]'.format(i+2))
-        next_page.click()
-    except:
-        next_page = "None"
+        links.append(job.find_element_by_xpath(
+            './/h2[@class="title"]//a').get_attribute(name="href"))
+        
 
-    print("Page: {}".format(str(i+2)))
+    print("Finish the page: {}.".format(str(i+1)))
+    driver.implicitly_wait(3)
+    next_page = driver.find_elements_by_xpath("//span[@class='pn']")[-1]
+    driver.execute_script("arguments[0].click();", next_page)
+    
+df = pd.DataFrame()
+df['Date'] = dates
+df['Link'] = links
 
-# Despcriptions
-for link in links:
-    try:
-        driver.get(link)
-        jd = driver.find_element_by_xpath(
-            '//div[@id="jobDescriptionText"]').text
-    except:
-        jd = "None"
-
-    descriptions.append(jd)
-
-
-df_da = pd.DataFrame()
-df_da['Title'] = titles
-df_da['Company'] = companies
-df_da['Location'] = locations
-df_da['Link'] = links
-df_da['Salary'] = salaries
-df_da['Date'] = dates
-df_da['Description'] = descriptions
-
-# save to feather file
-filename = datetime.now().strftime("%m_%d_%Y")
-df_da.to_feather("./source/"+filename)
-
-print("Done! " + filename)
+filename = datetime.now().strftime("Post_URL_"+"%m_%d_%Y")
+df.to_feather("./source/"+filename)
